414c2e7969179ba606a6ccdf3f615b683624dd56
Jeremy Xue <gg831006@gmail.com>
2019-04-15 09:24:15 +0800

    init project and add README

---
+++ Data update/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Data update
+//
+//  Created by JeremyXue on 2019/4/15.
+//  Copyright © 2019 JeremyXue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dataTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
